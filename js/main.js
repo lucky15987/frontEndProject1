0fbaab12334af71d5adf5e6c98709f196e016a22
Ethan N <lucky35789@yahoo.com>
2022-07-05 23:01:46 -0400

    added css animation to the "Searching now" overlay.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 //create an action for the "Find food now" button
 let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', () => {
-	
+	overlay.style.display = 'grid';
 })
